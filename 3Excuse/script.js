cfb6b0a0967df0685ea56ce833d60951f799b7ee
JamirGDC <gutierrez.dc@icloud.com>
2023-07-28 21:31:05 +0200

    practice3

---
+++ 3Excuse/script.js
@@ -0,0 +1,14 @@
+const parrafo = document.getElementById("excuse");
+const onload = document.getElementById("onload");
+
+let who = ['The dog', 'My grandma', 'His turtle', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'the keys', 'the car'];
+let when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
+
+function generateExcuse() {
+    parrafo.innerHTML = who[Math.floor(Math.random() * who.length)] + " " + action[Math.floor(Math.random() * action.length)] + " " + what[Math.floor(Math.random() * what.length)] + " " + when[Math.floor(Math.random() * when.length)];
+}
+
+addEventListener("load", generateExcuse);
+onload.addEventListener("click", generateExcuse);
